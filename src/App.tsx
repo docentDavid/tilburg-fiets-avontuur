73ff99fbbf72be5a41abe40b74784d1c20281834
David Schol <d.schol@fontys.nl>
2025-05-27 14:44:18 +0200

    Scroll added and white wave in hero deleted

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,34 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useScrollToTop } from "@/hooks/useScrollToTop";
 import Index from "./pages/Index";
 import AboutUs from "./pages/AboutUs";
 import Agenda from "./pages/Agenda";
 import NotFound from "./pages/NotFound";
-import InsertRides from "./pages/InsertRides"; // Add this import
+import InsertRides from "./pages/InsertRides";
 import Admin from "./pages/Admin";
 import UpdateDates from "./pages/UpdateDates";
 import UpdateDistances from "./pages/UpdateDistances";
 
 const queryClient = new QueryClient();
+
+const AppRoutes = () => {
+  useScrollToTop();
+
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/over-ons" element={<AboutUs />} />
+      <Route path="/agenda" element={<Agenda />} />
+      <Route path="/insert-rides" element={<InsertRides />} />
+      <Route path="/admin" element={<Admin />} />
+      <Route path="/update-dates" element={<UpdateDates />} />
+      <Route path="/update-distances" element={<UpdateDistances />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -20,18 +38,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/over-ons" element={<AboutUs />} />
-          <Route path="/agenda" element={<Agenda />} />
-          <Route path="/insert-rides" element={<InsertRides />} />{" "}
-          {/* Add this route */}
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/update-dates" element={<UpdateDates />} />
-          <Route path="/update-distances" element={<UpdateDistances />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutes />
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
